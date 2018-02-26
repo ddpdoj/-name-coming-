53ca8a7ce5a75aaacaac1685ed4c29e474d0e814
Nathan Keeney (09640) <NKeeney@us.pinkertons.com>
2018-02-26 07:33:15 -0800

    more

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { ModalModule } from 'ngx-bootstrap';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ModalModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
